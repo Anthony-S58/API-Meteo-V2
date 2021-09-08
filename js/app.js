c31eccee61e9ffcd2d25d78d04eccc0de5a60ce2
Anthony-S58 <tonysim01@hotmail.com>
2021-09-08 16:18:19 +0200

    newVersion

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// on insère la key API
+const APIKEY = '228c3d96bf64d2fbc24317ae62d42cf6' ;
+
+// appel à l'appi openWeather avec ville en paramètre de fonction
+let apiCall = function(city){
+
+
+// on récupère l'url dans la documentation du site 
+// units=metric correspond à la récupération de la température en celsius
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=fr&appid=${APIKEY}`;
+
+
+// on récupère les informations et on insère dans l'html
+fetch(url).then( (response) => response.json().then((data) => 
+    {console.log(data);
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temperature').innerHTML = "<i class='fas fa-thermometer-half'></i>" + data.main.temp.toFixed(1) + ' °';
+        document.querySelector('.humidity').innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + ' %';
+        document.querySelector('.wind').innerHTML = "<i class='fas fa-wind'></i>" + data.wind.speed + ' m/h';
+        document.querySelector('.description').innerHTML = data.weather[0].description.toUpperCase();
+        document.querySelector('.image').innerHTML = "<img src=http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png></img>";
+
+
+        
+    })
+).catch(err => console.log('Erreur : ' + err));
+};
+// écouteur d'évènement sur la soumission du formulaire
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    let ville = document.querySelector('#inputcity').value;
+
+    apiCall(ville);
+});
+
+// appel par défaut au chargement de la page
+apiCall('Nevers');
